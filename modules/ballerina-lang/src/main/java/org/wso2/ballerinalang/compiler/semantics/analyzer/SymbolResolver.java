4fe29d2cd2d9b859a86c1bed727d5a1ca0ac3fa3
Nadeeshaan <nadeeshaangunasinghe@gmail.com>
2017-09-23 15:43:53 +0530

    Core changes for Lang Server

--- modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolResolver.java
+++ modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolResolver.java
@@ -232,17 +232,17 @@ public class SymbolResolver extends BLangNodeVisitor {
         return symTable.notFoundSymbol;
     }
 
+    /**
+     * Lookup all the visible symbols for a given environment scope
+     * @param env Symbol environment
+     * @return all the visible symbols
+     */
     public Map<Name, ScopeEntry> lookupAllVisibleSymbols(SymbolEnv env) {
-        Map<Name, ScopeEntry> visibleEntries;
-
-        if (env.enclEnv == null) {
-            visibleEntries = new HashMap<>();
-        } else {
-            visibleEntries = Stream.concat(env.scope.entries.entrySet().stream(), lookupAllVisibleSymbols(env.enclEnv)
-                    .entrySet().stream())
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        Map<Name, ScopeEntry> visibleEntries = new HashMap<>();
+        visibleEntries.putAll(env.scope.entries);
+        if (env.enclEnv != null) {
+            visibleEntries.putAll(lookupAllVisibleSymbols(env.enclEnv));
         }
-
         return visibleEntries;
     }
 
